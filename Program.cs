69278a0d83413c920e4d597e738bd97c350846ac
AmirhosseinGharib <149072424+AmirhosseinGharib@users.noreply.github.com>
2023-11-01 14:10:25 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factoriel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, fact = 1;
+            Console.Write("please enter a number");
+            number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+                fact *= i;
+            Console.WriteLine(string.Format("The result is: {0}",fact));
+            Console.ReadLine();
+        }
+    }
+}
